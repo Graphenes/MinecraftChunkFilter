88c8b19f2b368b9be8a98fb0130a3c9b125b36ed
steve witherly <atomicletter@yahoo.com>
2018-09-28 05:43:42 -0500

    Added debug executable

--- MinecraftChunkFilter/Form1.Designer.vb
+++ MinecraftChunkFilter/Form1.Designer.vb
@@ -22,10 +22,88 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.TabControl1 = New System.Windows.Forms.TabControl()
+        Me.TabPage1 = New System.Windows.Forms.TabPage()
+        Me.TabPage2 = New System.Windows.Forms.TabPage()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.TextBox_LevelDatPath = New System.Windows.Forms.TextBox()
+        Me.Button_ChooseSavegame = New System.Windows.Forms.Button()
+        Me.TabControl1.SuspendLayout()
+        Me.TabPage2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'TabControl1
+        '
+        Me.TabControl1.Controls.Add(Me.TabPage1)
+        Me.TabControl1.Controls.Add(Me.TabPage2)
+        Me.TabControl1.Location = New System.Drawing.Point(12, 12)
+        Me.TabControl1.Name = "TabControl1"
+        Me.TabControl1.SelectedIndex = 0
+        Me.TabControl1.Size = New System.Drawing.Size(776, 426)
+        Me.TabControl1.TabIndex = 0
+        '
+        'TabPage1
+        '
+        Me.TabPage1.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage1.Name = "TabPage1"
+        Me.TabPage1.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage1.Size = New System.Drawing.Size(768, 400)
+        Me.TabPage1.TabIndex = 0
+        Me.TabPage1.Text = "TabPage1"
+        Me.TabPage1.UseVisualStyleBackColor = True
+        '
+        'TabPage2
+        '
+        Me.TabPage2.Controls.Add(Me.Button_ChooseSavegame)
+        Me.TabPage2.Controls.Add(Me.TextBox_LevelDatPath)
+        Me.TabPage2.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage2.Name = "TabPage2"
+        Me.TabPage2.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage2.Size = New System.Drawing.Size(768, 400)
+        Me.TabPage2.TabIndex = 1
+        Me.TabPage2.Text = "TabPage2"
+        Me.TabPage2.UseVisualStyleBackColor = True
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.DefaultExt = "dat"
+        Me.OpenFileDialog1.InitialDirectory = "Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)"
+        '
+        'TextBox_LevelDatPath
+        '
+        Me.TextBox_LevelDatPath.Location = New System.Drawing.Point(6, 6)
+        Me.TextBox_LevelDatPath.Name = "TextBox_LevelDatPath"
+        Me.TextBox_LevelDatPath.Size = New System.Drawing.Size(406, 20)
+        Me.TextBox_LevelDatPath.TabIndex = 0
+        '
+        'Button_ChooseSavegame
+        '
+        Me.Button_ChooseSavegame.Location = New System.Drawing.Point(418, 6)
+        Me.Button_ChooseSavegame.Name = "Button_ChooseSavegame"
+        Me.Button_ChooseSavegame.Size = New System.Drawing.Size(75, 23)
+        Me.Button_ChooseSavegame.TabIndex = 1
+        Me.Button_ChooseSavegame.Text = "Button1"
+        Me.Button_ChooseSavegame.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
+        Me.Controls.Add(Me.TabControl1)
+        Me.Name = "Form1"
+        Me.Text = "MinecraftChunkFilter - by Graphenes"
+        Me.TabControl1.ResumeLayout(False)
+        Me.TabPage2.ResumeLayout(False)
+        Me.TabPage2.PerformLayout()
+        Me.ResumeLayout(False)
+
     End Sub
 
+    Friend WithEvents TabControl1 As TabControl
+    Friend WithEvents TabPage1 As TabPage
+    Friend WithEvents TabPage2 As TabPage
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents Button_ChooseSavegame As Button
+    Friend WithEvents TextBox_LevelDatPath As TextBox
 End Class
--- MinecraftChunkFilter/Form1.vb
+++ MinecraftChunkFilter/Form1.vb
@@ -1,3 +1,23 @@
-﻿Public Class Form1
+﻿Imports Substrate
+Public Class Form1
 
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Form1_Shown(sender As Object, e As EventArgs) Handles Me.Shown
+
+    End Sub
+
+    Private Sub Button_ChooseSavegame_Click(sender As Object, e As EventArgs) Handles Button_ChooseSavegame.Click
+        OpenFileDialog1.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) & "\.minecraft\saves"
+        Dim result As DialogResult = OpenFileDialog1.ShowDialog()
+
+        ' Test result.
+        If result = Windows.Forms.DialogResult.OK Then
+            ' Get the file name.
+            Dim path As String = OpenFileDialog1.FileName
+            Me.TextBox_LevelDatPath.Text = path
+        End If
+    End Sub
 End Class
